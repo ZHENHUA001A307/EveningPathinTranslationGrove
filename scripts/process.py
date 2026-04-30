7b59c343014f27460b8d16ef7d36a0f37db58a7e
AntHarlov <59117052+ZHENHUA001A307@users.noreply.github.com>
2026-04-30 22:05:12 +0800

    Update process.py

--- scripts/process.py
+++ scripts/process.py
@@ -5,7 +5,7 @@ import google.generativeai as genai
 from pathlib import Path
 
 # --- 配置区 ---
-genai.configure(api_key=os.environ["AIzaSyBOogDSMLy-vBNfembDrmrGfUfOJUJNtCI"])
+genai.configure(api_key=os.environ["GEMINI_API_KEY"])
 model = genai.GenerativeModel('gemini-1.5-flash')
 
 def smart_split(text, chunk_size=10):
