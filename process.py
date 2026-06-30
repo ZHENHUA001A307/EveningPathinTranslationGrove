13757a35ea4251eb120654c8efe19fd35d433ff5
e3b0c44298fc1c149afbf4c <59117052+ZHENHUA001A307@users.noreply.github.com>
2026-06-30 10:50:19 +0800

    Update process.py

--- process.py
+++ process.py
@@ -22,7 +22,7 @@ LANG_CONFIG = {
     'en': {
         'input_file': 'input_en.csv',
         'progress_file': 'last_index_en.txt',
-        'batch_size': 25,           # 每次推送的句子数量上限
+        'batch_size': 40,           # 每次推送的句子数量上限
         'reading_speed': 150,       # 英语阅读速度（字符/分钟）
     },
     'jp': {
